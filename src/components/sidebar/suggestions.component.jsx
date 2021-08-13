de7f081ff2162c805be0d926a2363feed09a499d
caspero-62 <nwaforuchechukwu2007@gmail.com>
2021-08-13 03:55:21 +0100

    final touches

--- src/components/sidebar/suggestions.component.jsx
+++ src/components/sidebar/suggestions.component.jsx
@@ -7,11 +7,13 @@ import SuggestedProfile from './SuggestedProfile.component';
 
 const Suggestions = ({ userId, docId, following }) => {
     const [profiles, setProfiles] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const suggestedProfiles = async () => {
             const profiles  = await getSuggestedProfiles(userId, following);
-            setProfiles(profiles)
+            setProfiles(profiles);
+            setLoading(false);
         }
         if(userId){
             suggestedProfiles();
@@ -20,41 +22,44 @@ const Suggestions = ({ userId, docId, following }) => {
 
 
     return (
-        profiles && profiles.length > 0 ? (
-            <div className='w-full'>
-                <div className="flex justify-between mb-3">
-                    <p className='text-gray-faded text-xs font-bold'>Suggestions For You</p>
-                    <Link to="/explore/people/suggested">
-                        <p className='text-xs font-medium text-black-light'>See All</p>
-                    </Link>
-                </div>
-                <div>
-                    { profiles.map((profile) => {  
-                        return(
-                            <SuggestedProfile 
-                                fullName={profile.fullName}
-                                userName={profile.username} 
-                                loggedUserDocId={docId}
-                                spDocId={profile.docId}
-                                followers={profile.followers}
-                                email={profile.emailAddress} 
-                                key={profile.docId}
-                                profileId={profile.userId}
-                                userId={userId}
-                            />
+        <div>
+            {profiles && profiles.length > 0 && (
+                <div className='w-full'>
+                    <div className="flex justify-between mb-3">
+                        <p className='text-gray-faded text-xs font-bold'>Suggestions For You</p>
+                        <Link to="/explore/people/suggested">
+                            <p className='text-xs font-medium text-black-light'>See All</p>
+                        </Link>
+                    </div>
+                    <div>
+                        { profiles.map((profile) => {  
+                            return(
+                                <SuggestedProfile 
+                                    fullName={profile.fullName}
+                                    userName={profile.username} 
+                                    loggedUserDocId={docId}
+                                    spDocId={profile.docId}
+                                    followers={profile.followers}
+                                    email={profile.emailAddress} 
+                                    key={profile.docId}
+                                    profileId={profile.userId}
+                                    userId={userId}
+                                />
+                            )}
                         )}
-                    )}
+                    </div>
                 </div>
-            </div>
-        ): 
-        (
-            <>
-                <div className='mb-2'>
-                    <Skeleton count={1} height={20}/>
-                </div>
-                <Skeleton count={1} height={130}/>
-            </>
-        )
+            )}
+
+            {loading && (
+                <>
+                    <div className='mb-2'>
+                        <Skeleton count={1} height={20}/>
+                    </div>
+                    <Skeleton count={1} height={130}/>
+                </>
+            )}
+        </div>
     );
 };
 
--- src/seed.js
+++ src/seed.js
@@ -1,71 +1,84 @@
 // NOTE: replace 'b58cCbArfDRejQ4FXN3etRtEq7n2' with your Firebase auth user id (can be taken from Firebase)
 export function seedDatabase(firebase) {
-    const users = [
-    {
-        userId: 'b58cCbArfDRejQ4FXN3etRtEq7n2',
-        username: '__webdot',
-        fullName: 'Uchechuwku Nwafor',
-        emailAddress: 'nwaforuchechukwu2007@gmail.com',
-        following: ['2'],
-        followers: ['2', '3', '4'],
-        dateCreated: Date.now()
-    },
-    {
-        userId: '2',
-        username: 'snincode',
-        fullName: 'Abdulrazaq Sodiq',
-        emailAddress: 'razaqi@bleh.com',
-        following: [],
-        followers: ['b58cCbArfDRejQ4FXN3etRtEq7n2'],
-        dateCreated: Date.now()
-    },
-    {
-        userId: '3',
-        username: 'swiss_se',
-        fullName: 'Seun Awodele',
-        emailAddress: 'swiss@tooswiss.com',
-        following: [],
-        followers: ['b58cCbArfDRejQ4FXN3etRtEq7n2'],
-        dateCreated: Date.now()
-    },
-    {
-        userId: '4',
-        username: 'unknownthoth',
-        fullName: 'Shalom Tata',
-        emailAddress: 'thoth@trojan.com',
-        following: [],
-        followers: ['b58cCbArfDRejQ4FXN3etRtEq7n2'],
-        dateCreated: Date.now()
-    }
-    ];
+    // const users = [
+    // {
+    //     userId: 'b58cCbArfDRejQ4FXN3etRtEq7n2',
+    //     username: '__webdot',
+    //     fullName: 'Uchechuwku Nwafor',
+    //     emailAddress: 'nwaforuchechukwu2007@gmail.com',
+    //     following: ['2'],
+    //     followers: ['2', '3', '4'],
+    //     dateCreated: Date.now()
+    // },
+    // {
+    //     userId: '2',
+    //     username: 'snincode',
+    //     fullName: 'Abdulrazaq Sodiq',
+    //     emailAddress: 'razaqi@bleh.com',
+    //     following: [],
+    //     followers: ['b58cCbArfDRejQ4FXN3etRtEq7n2'],
+    //     dateCreated: Date.now()
+    // },
+    // {
+    //     userId: '3',
+    //     username: 'swiss_se',
+    //     fullName: 'Seun Awodele',
+    //     emailAddress: 'swiss@tooswiss.com',
+    //     following: [],
+    //     followers: ['b58cCbArfDRejQ4FXN3etRtEq7n2'],
+    //     dateCreated: Date.now()
+    // },
+    // {
+    //     userId: '4',
+    //     username: 'unknownthoth',
+    //     fullName: 'Shalom Tata',
+    //     emailAddress: 'thoth@trojan.com',
+    //     following: [],
+    //     followers: ['b58cCbArfDRejQ4FXN3etRtEq7n2'],
+    //     dateCreated: Date.now()
+    // }
+    // ];
 
-    for (let k = 0; k < users.length; k++) {
-        firebase.firestore().collection('users').add(users[k]);
-    }
+    // for (let k = 0; k < users.length; k++) {
+    //     firebase.firestore().collection('users').add(users[k]);
+    // }
 
-    for (let i = 1; i <= 5; ++i) {
+    for (let i = 1; i <= 2; ++i) {
     firebase
         .firestore()
         .collection('photos')
         .add({
-        photoId: i,
-        userId: '2',
-        imageSrc: `/images/users/uche/${i}.jpg`,
-        caption: '🐐🐐🐐🐐',
+        photoId: i*8,
+        userId: '4',
+        imageSrc: `/images/users/tata/${i}.jpg`,
+        caption: 'Never forget',
         likes: [],
         comments: [
-            {
-                displayName: '__webdot',
-                comment: 'Look at curry man!! so inspirational'
-            },
-            {
-                displayName: 'swiss_se',
-                comment: 'Greatest shooter of all time!!'
-            }
+            
         ],
-        userLatitude: '40.7128°',
-        userLongitude: '74.0060°',
+        userLatitude: '540.7128°',
+        userLongitude: '740.0060°',
         dateCreated: Date.now()
         });
     }
+
+    for (let i = 1; i <= 3; ++i) {
+    firebase
+        .firestore()
+        .collection('photos')
+        .add({
+        photoId: i*20,
+        userId: 'b58cCbArfDRejQ4FXN3etRtEq7n2',
+        imageSrc: `/images/users/uche/${i}.jpg`,
+        caption: 'Hello there',
+        likes: [],
+        comments: [
+            
+        ],
+        userLatitude: '54.7137°',
+        userLongitude: '90.0160°',
+        dateCreated: Date.now()
+        });
+    }
+    
 }  
